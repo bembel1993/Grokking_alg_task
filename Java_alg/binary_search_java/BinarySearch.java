bd04c94a480f778ccdb68cab0e0090447044af48
Vitali <bembel1.20@gmail.com>
2025-07-16 22:13:01 +0300

    2

--- Java_alg/binary_search_java/BinarySearch.java
+++ Java_alg/binary_search_java/BinarySearch.java
@@ -4,6 +4,35 @@ public class BinarySearch
 {
     public static void main (String args[])
     {
-        
+        int[] arr = {1, 3, 5, 7, 9};
+
+        BinarySearchFunction binarySearchFunction = new BinarySearchFunction();
+        System.out.println(binarySearchFunction.binary_search(arr, 3));
+        System.out.println(binarySearchFunction.binary_search(arr, -1));
     }
+}
+
+class BinarySearchFunction 
+{
+    public int binary_search (int[] arr, int item) 
+    {
+        int low = 0;
+        int high = arr.length - 1;
+
+        while(low <= high)
+        {
+            int mid = (low + high) / 2;
+            int guess = arr[mid];
+
+            if (guess == item)
+            {
+                return mid;
+            } else if(guess > item){
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return -1;
+    } 
 }
